eb07ac4ca581d01f927cccd2ef91524a4fc02ae0
dorlens janvier <dorlensjanvier@MacBookAir.home>
2025-08-14 18:46:23 -0400

    Updated tello code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from djitellopy import Tello
 from pynput import keyboard
 import threading
 
+
 tello = Tello()
 tello.connect()
 print(f"Battery: {tello.get_battery()}%")
@@ -11,41 +12,29 @@ Arrow_press_dist = 30
 def controller(key):
     if key.char == "t":
         tello.takeoff()
-    # elif key == keyboard.Key.up:
-    #     tello.move_forward(Arrow_press_dist)
-    # elif key == keyboard.Key.down:
-    #     tello.move_back(Arrow_press_dist)
-    # elif key == keyboard.Key.left:
-    #     tello.move_left(Arrow_press_dist)
-    # elif key == keyboard.Key.right:
-    #     tello.move_right(Arrow_press_dist)
+    elif key.char =="l":
+       tello.move_left(Arrow_press_dist)
+    elif key.char =="r":
+       tello.move_right(Arrow_press_dist)
+    elif key.char =="b":
+         tello.move_back(Arrow_press_dist)
+    elif key.char == 'f':
+        tello.move_forward(Arrow_press_dist)  
     elif key.char == 'u':
         tello.move_up(Arrow_press_dist)
     elif key.char == 'd':
         tello.move_down(Arrow_press_dist)
-    # elif key == keyboard.Key.space:
-    #     tello.land()
-
-    else:
-     if key == keyboard.Key.up:
-        tello.move_forward(Arrow_press_dist)
-     elif key == keyboard.Key.down:
-        tello.move_back(Arrow_press_dist)
-     elif key == keyboard.Key.left:
-        tello.move_left(Arrow_press_dist)
-     elif key == keyboard.Key.right:
-        tello.move_right(Arrow_press_dist)
-     elif key == keyboard.Key.space:
-      tello.land()
+    elif key.char == 'q':
+        tello.land()
 with keyboard.Listener(on_press=controller) as listener:
     listener.join()
 
 print("Controls:")
 print("t - Takeoff")
-print("Up Arrow - Forward")
-print("Down Arrow - Backward")
-print("Left Arrow - Left")
-print("Right Arrow - Right")
+print("u- Forward")
+print("d - Backward")
+print("l - Left")
+print("r - Right")
 print("u - Up")
 print("d - Down")
-print("Space - Land")
+print("q- Land")
